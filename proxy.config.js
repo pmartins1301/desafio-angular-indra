2eaaeb38d534641575fb975fe553d5419abb42a7
Pedro Yvo <62101651+pmartins1301@users.noreply.github.com>
2021-04-28 22:29:09 -0300

    Add files via upload

---
+++ proxy.config.js
@@ -0,0 +1,9 @@
+// const proxy = [
+//     {
+//       context: ['/api'],
+//       target: 'https://combustivelapp.herokuapp.com/',
+//       secure: true,
+//       pathRewrite: {'^/api' : ''}
+//     }
+//   ];
+//   module.exports = proxy;
